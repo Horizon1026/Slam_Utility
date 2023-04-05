127d2dd80a4a10458dd81da6df0ad953928d82d6
root <root@localhost.localdomain>
2023-04-05 08:17:28 +0000

    add find_eigen3.cmake

---
+++ src/datatype/cmake_modules/FineEigen3.cmake
@@ -0,0 +1,12 @@
+# Try to find eigen3.
+
+if ( EIGEN3_INCLUDE_DIR )
+	set( EIGEN3_INCLUDE_DIR )
+else ( EIGEN3_INCLUDE_DIR )
+	find_path( EIGEN3_INCLUDE_DIR NAMES Eigen/Core
+		PATH_SUFFIXES eigen3/
+        HINTS         ${INCLUDE_INSTALL_DIR}
+        			  /usr/local/include
+                      ${KDE4_INCLUDE_DIR}
+    )
+endif ( EIGEN3_INCLUDE_DIR )
