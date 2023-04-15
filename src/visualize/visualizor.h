3b15fac8f5c79deb1419d82633aa2857494733c3
Horizon1026 <lijiapeng_atlantis@163.com>
2023-04-15 20:50:56 +0800

    slam utility

--- src/visualize/visualizor.h
+++ src/visualize/visualizor.h
@@ -1,6 +1,9 @@
 #ifndef _SLAM_UTILITY_VISUALIZOR_H_
 #define _SLAM_UTILITY_VISUALIZOR_H_
 
+#include "datatype_basic.h"
+#include "datatype_image.h"
+
 namespace SLAM_UTILITY {
 
 class Visualizor {
