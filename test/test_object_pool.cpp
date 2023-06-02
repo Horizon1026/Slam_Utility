9919e7c0624a4682a3fdb035fa2d8b9bd5c5cfe2
Horizon1026 <lijiapeng_atlantis@163.com>
2023-06-02 16:25:37 +0800

    slam utility

--- test/test_object_pool.cpp
+++ test/test_object_pool.cpp
@@ -1,22 +1,23 @@
 #include "log_report.h"
 #include "object_pool.h"
+#include "datatype_basic.h"
 
 using namespace SLAM_UTILITY;
 
 int main(int argc, char **argv) {
     ReportInfo(YELLOW ">> Test object pool." RESET_COLOR);
 
-    ObjectPool<double> pool(10);
+    ObjectPool<Mat1> pool(10);
     {
-    	std::vector<ObjectPtr<double>> objects;
+    	std::vector<ObjectPtr<Mat1>> objects;
 		for (int32_t i = 0; i < 10; ++i) {
             auto ptr = pool.Get();
     	    objects.emplace_back(std::move(ptr));
-		    *objects.back().get() = 1.0 + static_cast<double>(i);
+		    *objects.back().get() = Mat1::Identity() * (1 + i);
     	}
     }
 
-    std::vector<ObjectPtr<double>> objects;
+    std::vector<ObjectPtr<Mat1>> objects;
 	for (int32_t i = 0; i < 10; ++i) {
         objects.emplace_back(pool.Get());
         ReportInfo("Item in object pool is " << *objects.back().get());
