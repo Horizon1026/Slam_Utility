aa3a51f19838aff5b02763c44b3c7f636af5c717
Horizon1026 <lijiapeng_atlantis@163.com>
2023-06-29 23:49:17 +0800

    slam utility

--- src/visualize/visualizor.cpp
+++ src/visualize/visualizor.cpp
@@ -4,6 +4,10 @@ namespace SLAM_UTILITY {
 
 bool Visualizor::ShowImage(const std::string &window_title, const Image &image) {
 
+    // If the window of the same title is exist, just refresh its image.
+
+    // Create new window, and show image.
+
     return true;
 }
 }
