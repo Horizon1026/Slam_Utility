6f26aa15977c037aa03fa88672b2af29b04db950
Horizon1026 <lijiapeng_atlantis@163.com>
2023-07-12 10:02:05 +0800

    slam utility

--- src/visualize/visualizor_draw_image.cpp
+++ src/visualize/visualizor_draw_image.cpp
@@ -25,7 +25,45 @@ template void Visualizor::DrawBressenhanLine<GrayImage, uint8_t>(GrayImage &imag
 template void Visualizor::DrawBressenhanLine<RgbImage, RgbPixel>(RgbImage &image, int32_t x1, int32_t y1, int32_t x2, int32_t y2, const RgbPixel &color);
 template <typename ImageType, typename PixelType>
 void Visualizor::DrawBressenhanLine(ImageType &image, int32_t x1, int32_t y1, int32_t x2, int32_t y2, const PixelType &color) {
+    if (image.data() == nullptr) {
+        return;
+    }
 
+    const int32_t dx = std::abs(x2 - x1);
+    const int32_t dy = std::abs(y2 - y1);
+    bool is_larger_than_45_deg = false;
+    if (dx < dy) {
+        is_larger_than_45_deg = true;
+        std::swap(x1, y1);
+        std::swap(x2, y2);
+        std::swap(dx, dy);
+    }
+
+    // Precompute some temp variables.
+    const int32_t ix = (x2 - x1) > 0 ? 1 : -1;
+    const int32_t iy = (y2 - y1) > 0 ? 1 : -1;
+    int32_t cx = x1;
+    int32_t cy = y1;
+    const int32_t dy_2 = dy << 1;
+    const int32_t dy_dx_2 = (dy - dx) << 1;
+    int32_t dy_2_dx = dy << 1 - dx;
+
+    // Draw line.
+    while (cx != x2) {
+        if (dy_2_dx < 0) {
+            dy_2_dx += dy_2;
+        } else {
+            cy += iy;
+            dy_2_dx += dy_dx_2;
+        }
+
+        if (is_larger_than_45_deg) {
+            image.SetPixelValue(cy, cx, color);
+        } else {
+            image.SetPixelValue(cx, cy, color);
+        }
+        cx += ix;
+    }
 }
 
 }
