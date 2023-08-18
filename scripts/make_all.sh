fb97ddc97a488b3714e8581542da070f5ccbee2d
Horizon1026 <lijiapeng_atlantis@163.com>
2023-08-19 04:54:26 +0800

    update code

--- scripts/make_all.sh
+++ scripts/make_all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-while read rows
-do
+# 要去掉'\r'符号，所以写成下面这样
+while IFS=$'\r' read -r rows; do
     sh make.sh $rows $1
 done < all_repos_name.txt
--- scripts/push_all_repo.sh
+++ scripts/push_all_repo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-while read rows
-do
+# 要去掉'\r'符号，所以写成下面这样
+while IFS=$'\r' read -r rows; do
     sh push_repo.sh "update code" $rows
 done < all_repos_name.txt
