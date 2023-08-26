607732796e55656f2a491032e5c55d2140f906fa
Horizon1026 <lijiapeng_atlantis@163.com>
2023-08-27 01:27:27 +0800

    update code

---
+++ temp_files/Binary_Data_Viewer/ReadMe.md
@@ -0,0 +1,12 @@
+# Binary Data Viewer
+Simple data viewer for binary data log.
+
+# Components
+- [x] Viewer Dependence - ImGui.
+
+# Dependence
+Slam_Utility, Binary_Data_Log
+
+# Tips
+- 欢迎一起交流学习，不同意商用；
+- 仓库 owner 为 Horizon1026，暂无共同开发者，请勿 merge；
---
+++ temp_files/Binary_Data_Viewer/test/test_viewer.cpp
@@ -0,0 +1,12 @@
+#include "datatype_basic.h"
+#include "datatype_image.h"
+#include "log_report.h"
+#include "slam_operations.h"
+
+#include "iostream"
+
+int main(int argc, char **argv) {
+    ReportInfo(YELLOW ">> Test binary data viewer." RESET_COLOR);
+
+    return 0;
+}
