c556f47c16d39dced7307618ef9a12d9b06be52e
Horizon1026 <lijiapeng_atlantis@163.com>
2023-09-13 14:43:55 +0800

    update code

---
+++ test/test_backward.cpp
@@ -0,0 +1,11 @@
+#include "datatype_basic.h"
+#include "backward.h"
+#include "log_report.h"
+
+using namespace SLAM_UTILITY;
+
+int main(int argc, char **argv) {
+    ReportInfo(YELLOW ">> Test stack trace back." RESET_COLOR);
+
+    return 0;
+}
