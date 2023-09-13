0c9991958d411f63557c62ef0a82579d455b07d9
Horizon1026 <lijiapeng_atlantis@163.com>
2023-09-14 00:19:26 +0800

    update code

--- test/test_backward.cpp
+++ test/test_backward.cpp
@@ -1,11 +1,19 @@
 #include "datatype_basic.h"
 #include "backward.h"
 #include "log_report.h"
+#include "vector"
 
 using namespace SLAM_UTILITY;
 
+backward::SignalHandling sh;
+
 int main(int argc, char **argv) {
     ReportInfo(YELLOW ">> Test stack trace back." RESET_COLOR);
+
+    std::vector<float> numbers;
+    for (uint32_t i = 0; i <= 10; ++i) {
+        ReportInfo(numbers.front());
+    }
 
     return 0;
 }
