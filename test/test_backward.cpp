73ee5fb9cf6604991cbf10441849d221653fd957
Horizon1026 <lijiapeng_atlantis@163.com>
2023-09-16 00:49:01 +0800

    update code

--- test/test_backward.cpp
+++ test/test_backward.cpp
@@ -5,7 +5,9 @@
 
 using namespace SLAM_UTILITY;
 
+#ifndef _WIN32
 backward::SignalHandling sh;
+#endif
 
 int main(int argc, char **argv) {
     ReportInfo(YELLOW ">> Test stack trace back." RESET_COLOR);
