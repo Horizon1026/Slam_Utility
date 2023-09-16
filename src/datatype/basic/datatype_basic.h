ae61fd13177a77b7d69d1775accf92759aee99ae
Horizon1026 <lijiapeng_atlantis@163.com>
2023-09-17 01:07:59 +0800

    update code

--- src/datatype/basic/datatype_basic.h
+++ src/datatype/basic/datatype_basic.h
@@ -10,7 +10,7 @@ using uint8_t = unsigned char;
 using uint16_t = unsigned short;
 using uint32_t = unsigned int;
 
-using int8_t = char;
+using int8_t = signed char;
 using int16_t = short;
 using int32_t = int;
 
