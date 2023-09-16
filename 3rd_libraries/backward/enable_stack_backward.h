5da9fb5a8559a65cbb347ce8acb3945304b89c2f
Horizon1026 <lijiapeng_atlantis@163.com>
2023-09-16 19:30:52 +0800

    update code

---
+++ 3rd_libraries/backward/enable_stack_backward.h
@@ -0,0 +1,5 @@
+#ifndef _WIN32
+#define BACKWARD_HAS_DW 1
+#include "backward.h"
+backward::SignalHandling sh;
+#endif
--- test/test_backward.cpp
+++ test/test_backward.cpp
@@ -1,18 +1,16 @@
 #include "datatype_basic.h"
-#include "backward.h"
 #include "log_report.h"
 #include "vector"
 
 using namespace SLAM_UTILITY;
 
-#ifndef _WIN32
-backward::SignalHandling sh;
-#endif
+#include "enable_stack_backward.h"
 
 int main(int argc, char **argv) {
     ReportInfo(YELLOW ">> Test stack trace back." RESET_COLOR);
 
-    throw 1;
+    std::vector<float> temp_vec;
+    temp_vec[1] = 6.0f;
 
     return 0;
 }
