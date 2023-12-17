9a8730315f516c0f06513b3e6cbb155e37d35284
Horizon1026 <lijiapeng_atlantis@163.com>
2023-12-17 22:44:35 +0800

    update code

--- src/data_struct/covisible_graph/visual_feature.h
+++ src/data_struct/covisible_graph/visual_feature.h
@@ -52,7 +52,7 @@ private:
     std::vector<ObserveType> observes_;
 
     // Feature parameter can be solved. For example, feature point's parameter type is Eigen::Vector3f.
-    ParamType param_;
+    ParamType param_ = ParamType::Zero();
 
     // Solved type of this feature.
     FeatureSolvedStatus status_ = FeatureSolvedStatus::kUnsolved;
