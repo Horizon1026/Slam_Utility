30a4683ac5095677ffdd50c6e64c0ba556dc8b0a
Horizon1026 <lijiapeng_atlantis@163.com>
2024-04-24 00:31:06 +0800

    update code

--- test/test_libtorch.cpp
+++ test/test_libtorch.cpp
@@ -2,30 +2,45 @@
 #include "libtorch.h"
 
 struct Net : torch::nn::Module {
-    Net() {
-        fc1 = register_module("fc1", torch::nn::Linear(784, 64));
-        fc2 = register_module("fc2", torch::nn::Linear(64, 32));
-        fc3 = register_module("fc3", torch::nn::Linear(32, 10));
+    Net() :
+        conv1(torch::nn::Conv2dOptions(1, 2, /*kernel_size=*/3)),
+        conv2(torch::nn::Conv2dOptions(2, 4, /*kernel_size=*/3)),
+        fc1(576, 128),
+        fc2(128, 10) {
+        register_module("conv1", conv1);
+        register_module("conv2", conv2);
+        register_module("conv2_drop", conv2_drop);
+        register_module("fc1", fc1);
+        register_module("fc2", fc2);
     }
 
     torch::Tensor forward(torch::Tensor x) {
-        x = torch::relu(fc1->forward(x.reshape({x.size(0), 784})));
+        // input : 1*28*28.
+        // conv1 : 28 - 3 + 1 = 26.
+        x = torch::relu(conv1->forward(x));
+        // input : 26*26*2.
+        // conv2 : 26 - 3 + 1 = 24.
+        // max_pool : 12 * 12 * 4 = 576.
+        x = torch::max_pool2d(torch::relu(conv2->forward(x)), 2);
+        x = torch::dropout(x, 0.25, is_training());
+        x = x.view({-1, 576});
+        // w : 128 * 576.
+        x = torch::relu(fc1->forward(x));
         x = torch::dropout(x, 0.5, is_training());
-        x = torch::relu(fc2->forward(x));
-        x = torch::log_softmax(fc3->forward(x), 1);
+        // w : 10 * 128.
+        x = fc2->forward(x);
+        x = torch::log_softmax(x, 1);
         return x;
     }
 
-    torch::nn::Linear fc1{nullptr};
-    torch::nn::Linear fc2{nullptr};
-    torch::nn::Linear fc3{nullptr};
+    torch::nn::Conv2d conv1;
+    torch::nn::Conv2d conv2;
+    torch::nn::Dropout conv2_drop;
+    torch::nn::Linear fc1;
+    torch::nn::Linear fc2;
 };
 
-int main(int argc, char **argv) {
-    ReportInfo(YELLOW ">> Test libtorch." RESET_COLOR);
-
-    auto net = std::make_shared<Net>();
-
+void Train(std::shared_ptr<Net> &net) {
     auto data_loader = torch::data::make_data_loader(
         torch::data::datasets::MNIST("/mnt/d/My_Github/Datasets/Mnist").map(
             torch::data::transforms::Stack<>()
@@ -50,11 +65,41 @@ int main(int argc, char **argv) {
             optimizor.step();
             // Print the loss and check point.
             if (++batch_index % 100 == 0) {
-                ReportInfo("epoch [" << epoch << "], batch_idx [" << batch_index << "], loss [" << loss.item<float>() << "].");
+                ReportInfo("[Train] epoch [" << epoch << "], batch_idx [" << batch_index << "], loss [" << loss.item<float>() << "].");
                 torch::save(net, "net.pt");
             }
         }
     }
+}
+
+void Test(std::shared_ptr<Net> &net) {
+    auto data_loader = torch::data::make_data_loader(
+        torch::data::datasets::MNIST("/mnt/d/My_Github/Datasets/Mnist", torch::data::datasets::MNIST::Mode::kTest).map(
+            torch::data::transforms::Stack<>()
+        ), 64
+    );
+
+    float total_loss = 0.0;
+    int32_t correct_cnt = 0;
+    for (auto &batch : *data_loader) {
+        // Execute the model on the input data.
+        torch::Tensor output = net->forward(batch.data);
+        // Compute a loss value to judge the output of our model.
+        torch::Tensor loss = torch::nll_loss(output, batch.target);
+        total_loss += loss.item<float>();
+        // Check correction.
+        torch::Tensor prediction = output.argmax(1);
+        correct_cnt += prediction.eq(batch.target).sum().item<int32_t>();
+    }
+
+}
+
+int main(int argc, char **argv) {
+    ReportInfo(YELLOW ">> Test libtorch." RESET_COLOR);
+
+    auto net = std::make_shared<Net>();
+    Train(net);
+    Test(net);
 
     return 0;
 }
