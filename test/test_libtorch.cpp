c4830dc50be15ad5b1ef9594ef5520279118bf9e
Horizon1026 <lijiapeng_atlantis@163.com>
2024-04-27 23:37:28 +0800

    update code

--- test/test_libtorch.cpp
+++ test/test_libtorch.cpp
@@ -9,7 +9,6 @@ struct Model : torch::nn::Module {
         fc2(128, 10) {
         register_module("conv1", conv1);
         register_module("conv2", conv2);
-        register_module("conv2_drop", conv2_drop);
         register_module("fc1", fc1);
         register_module("fc2", fc2);
     }
@@ -22,11 +21,9 @@ struct Model : torch::nn::Module {
         // conv2 : 24 - 3 + 1 = 22.
         // max_pool : 11 * 11 * 4 = 484.
         x = torch::max_pool2d(torch::relu(conv2->forward(x)), 2);
-        x = torch::dropout(x, /*p=*/0.25, is_training());
         x = x.view({-1, 484});
         // w : 128 * 484.
         x = torch::relu(fc1->forward(x));
-        x = torch::dropout(x, /*p=*/0.5, is_training());
         // w : 10 * 128.
         x = fc2->forward(x);
         x = torch::log_softmax(x, 1);
@@ -35,7 +32,6 @@ struct Model : torch::nn::Module {
 
     torch::nn::Conv2d conv1;
     torch::nn::Conv2d conv2;
-    torch::nn::Dropout conv2_drop;
     torch::nn::Linear fc1;
     torch::nn::Linear fc2;
 };
